5560821f7bbbb07b16c91fb0e20b1a7ccb1b3d07
jaredhodgkins <jared.hodgkins@gmail.com>
2019-10-08 03:15:13 -0400

    Part 5 - (Test) was_published_recently() returns False on future tests. [Fixed the test to test for future pub_date

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -13,6 +13,6 @@ class QuestionModelTests(TestCase):
         was_published_recently() returns False for questions whose pub_date
         is in the future
         """
-        time = timezone.now()
+        time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
